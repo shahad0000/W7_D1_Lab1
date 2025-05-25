7939569f0b5e1bf4b08717af70e99b15e2a4f2e8
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-25 10:55:00 +0300

    improve UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,56 +4,69 @@ import "./App.css";
 function App() {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
-const [result, setResult] = useState(0)
+  const [result, setResult] = useState(0);
   const increase1 = (num) => {
-    
-    setNum1(num1 + 1)
-  }
+    setNum1(num1 + 1);
+  };
 
   const decrease1 = () => {
-    setNum1(num1 - 1)
-  }
+    setNum1(num1 - 1);
+  };
 
   const increase2 = (num) => {
-    
-    setNum2(num2 + 1)
-  }
+    setNum2(num2 + 1);
+  };
 
   const decrease2 = () => {
-    setNum2(num2 - 1)
-  }
+    setNum2(num2 - 1);
+  };
 
-const addition = () => {
-  setResult(() => num1 + num2)
-}
-const substraction = () => {
-  setResult(() => num1 - num2)
-}
-const multiplication = () => {
-  setResult(() => num1 * num2)
-}
-const division = () => {
-  setResult(() => num1 / num2)
-}
-
+  const addition = () => {
+    setResult(() => num1 + num2);
+  };
+  const substraction = () => {
+    setResult(() => num1 - num2);
+  };
+  const multiplication = () => {
+    setResult(() => num1 * num2);
+  };
+  const division = () => {
+    setResult(() => num1 / num2);
+  };
 
   return (
     <>
-      <div className="flex justify-around items-center">
+      <div className="flex flex-col justify-center items-center gap-3 p-5">
         <div>First value: {num1}</div>
-        <button onClick={increase1}>Increase +</button>
-        <button onClick={decrease1}>Decrease -</button>
+        <div className="flex gap-4">
+          <button className="border bg-blue-300" onClick={increase1}>
+            Increase +
+          </button>
+          <button className="border bg-red-200" onClick={decrease1}>
+            Decrease -
+          </button>
+        </div>
 
         <div>Second value: {num2}</div>
-        <button onClick={increase2}>Increase +</button>
-        <button onClick={decrease2}>Decrease -</button>
+        <div className="flex gap-4">
+          <button className="border bg-blue-300" onClick={increase2}>
+            Increase +
+          </button>
+          <button className="border bg-red-200" onClick={decrease2}>
+            Decrease -
+          </button>
+        </div>
       </div>
-      <div>Result {result}</div>
-      <button onClick={addition}>Add </button>
-      <button onClick={substraction}>substract </button>
-      <button onClick={multiplication}>Multiply </button>
-      <button onClick={division}>Divide</button>
-
+      <br />
+      <div className="flex flex-col items-center gap-4">
+        <div>Result {result}</div>
+        <div className="flex gap-5">
+          <button className="border bg-green-200" onClick={addition}>Add </button>
+          <button className="border bg-green-200" onClick={substraction}>substract </button>
+          <button className="border bg-green-200" onClick={multiplication}>Multiply </button>
+          <button className="border bg-green-200" onClick={division}>Divide</button>
+        </div>
+      </div>
     </>
   );
 }
